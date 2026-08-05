02bee81abc0eb3980b1a023dcc147de9182454e4
Rishit Swain <rajashreeswain471@gmail.com>
2026-08-06 01:48:07 +0530

    Add Single Number solution in SingleNumber.java
    Implement solution for LeetCode problem 136 using bit manipulation.

---
+++ Bit_Manipulation/SingleNumber.java
@@ -0,0 +1,20 @@
+/*
+LeetCode 136 - Single Number
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public int singleNumber(int[] nums) {
+
+        int ans = 0;
+
+        for (int num : nums) {
+            ans ^= num;
+        }
+
+        return ans;
+    }
+}
