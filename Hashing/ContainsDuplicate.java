06a8e8dbcdef6d4cb65cd1e0654655b76c4239f0
Rishit Swain <rajashreeswain471@gmail.com>
2026-08-07 20:32:00 +0530

    Add ContainsDuplicate solution using HashSet
    Implemented a solution to check for duplicate elements in an array using a HashSet.

---
+++ Hashing/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+/*
+LeetCode 217 - Contains Duplicate
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+}
